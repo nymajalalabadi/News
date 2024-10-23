d97089ae3161059ccf435b07905bbcb2a8f88f95
nymajalalabadi <nima.jalalabadii@gmail.com>
2024-10-22 21:01:12 -0600

    Added Norification For Admin

---
+++ News.Application/Services/Implementations/ReportService.cs
@@ -0,0 +1,31 @@
+﻿using News.Application.Services.Interfaces;
+using News.DataLayer.Context;
+using News.Domain.InterFaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace News.Application.Services.Implementations
+{
+    public class ReportService : IReportService
+    {
+        #region Constructor
+
+        private readonly IReportReporistory _reportReporistory;
+
+        public ReportService(IReportReporistory reportReporistory)
+        {
+            _reportReporistory = reportReporistory;
+        }
+
+        #endregion
+
+        #region Methods
+
+
+
+        #endregion
+    }
+}
---
+++ News.DataLayer/Repositories/ReportReporistory.cs
@@ -0,0 +1,30 @@
+﻿using News.DataLayer.Context;
+using News.Domain.InterFaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace News.DataLayer.Repositories
+{
+    public class ReportReporistory : IReportReporistory
+    {
+        #region Constructor
+
+        private readonly NewsDbContext _context;
+
+        public ReportReporistory(NewsDbContext context)
+        {
+            _context = context;
+        }
+
+        #endregion
+
+        #region Methods
+
+
+
+        #endregion
+    }
+}
