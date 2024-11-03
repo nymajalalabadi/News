1dfdd964758a5b26451c825820433d17f97d1952
nymajalalabadi <nima.jalalabadii@gmail.com>
2024-11-03 14:32:02 -0600

    Added SocialViewComponent

---
+++ News.Web/ViewComponents/NavTabComponent.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using News.Application.Services.Interfaces;
+
+namespace News.Web.ViewComponents
+{
+    #region Social
+
+    public class SocialViewComponent : ViewComponent
+    {
+        #region consractor
+
+        private readonly IReportService _reportService;
+
+        public SocialViewComponent(IReportService reportService)
+        {
+            _reportService = reportService;
+        }
+
+        #endregion
+
+        #region Method
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var data = await _reportService.GetReportsForIndex("Social");
+
+            return View("SocialViewComponent", data);
+        }
+
+        #endregion
+    }
+    #endregion
+}
