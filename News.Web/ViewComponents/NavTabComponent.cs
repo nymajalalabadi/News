212cc177f99458eb61f0e35ee670b769c84048b2
nymajalalabadi <nima.jalalabadii@gmail.com>
2024-11-03 15:05:23 -0600

    Added PoliticalViewComponent

--- News.Web/ViewComponents/NavTabComponent.cs
+++ News.Web/ViewComponents/NavTabComponent.cs
@@ -30,4 +30,63 @@ namespace News.Web.ViewComponents
         #endregion
     }
     #endregion
+
+    #region Cultural
+
+    public class CulturalViewComponent : ViewComponent
+    {
+        #region consractor
+
+        private readonly IReportService _reportService;
+
+        public CulturalViewComponent(IReportService reportService)
+        {
+            _reportService = reportService;
+        }
+
+        #endregion
+
+        #region Method
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var data = await _reportService.GetReportsForIndex("Cultural");
+
+            return View("CulturalViewComponent", data);
+        }
+
+        #endregion
+
+    }
+
+    #endregion
+
+    #region Political
+
+    public class PoliticalViewComponent : ViewComponent
+    {
+        #region consractor
+
+        private readonly IReportService _reportService;
+
+        public PoliticalViewComponent(IReportService reportService)
+        {
+            _reportService = reportService;
+        }
+
+        #endregion
+
+        #region Method
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var data = await _reportService.GetReportsForIndex("Political");
+
+            return View("PoliticalViewComponent", data);
+        }
+
+        #endregion
+    }
+
+    #endregion
 }
