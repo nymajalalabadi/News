349666b47c30ea4cea8c60508a9b0c7435385f6d
nymajalalabadi <nima.jalalabadii@gmail.com>
2024-11-24 21:14:18 -0600

    Added Delete Image Json

--- News.Web/wwwroot/Admin/assets/js/admin-custom.js
+++ News.Web/wwwroot/Admin/assets/js/admin-custom.js
@@ -222,6 +222,57 @@ function DeleteGallery(id) {
         });
 }
 
+function DeleteImage(id) {
+    swal({
+        title: "آیا مطمئن هستی ؟",
+        text: "در صورت انجام این عملیات قادر به بازگردانی آن نمی باشید.",
+        icon: "warning",
+        buttons: true,
+        dangerMode: true,
+    })
+        .then((willDelete) => {
+            if (willDelete) {
+                $.ajax({
+                    url: "/admin/gallery/DeleteImage",
+                    type: "post",
+                    data: {
+                        id: id
+                    },
+                    beforeSend: function () {
+
+                    },
+                    success: function (response) {
+                        if (response.status === "error") {
+                            swal({
+                                title: "خطا",
+                                text: response.message,
+                                icon: "error",
+                                button: "باشه"
+                            });
+                        }
+                        else {
+                            $(`#image-row-${id}`).fadeOut(500);
+                            swal({
+                                title: "اعلان",
+                                text: response.message,
+                                icon: "success",
+                                button: "باشه"
+                            });
+                        }
+                    },
+                    error: function () {
+                        swal({
+                            title: "خطا",
+                            text: "عملیات با خطا مواجه شد لطفا مجدد تلاش کنید .",
+                            icon: "error",
+                            button: "باشه"
+                        });
+                    }
+                });
+            }
+        });
+}
+
 /////ckeditor
 
 $(document).ready(function () {
