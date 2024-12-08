19658b99a2437862c5799c70d5ff5d7a8b895e1d
nym jalalabadi <nima.jalalabadii@gmail.com>
2024-12-07 21:40:33 -0600

    Added Menu Script Partial

---
+++ News.Web/Components/SiteMenoViewComponent.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using News.Application.Services.Interfaces;
+
+namespace News.Web.Components
+{
+    [ViewComponent(Name = "SiteMenoViewComponent")]
+    public class SiteMenoViewComponent : ViewComponent
+    {
+        #region consractor
+
+        private readonly IGalleryService _galleryService;
+
+        public SiteMenoViewComponent(IGalleryService galleryService)
+        {
+            _galleryService = galleryService;
+        }
+
+        #endregion
+
+        #region Method
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var gallery = await _galleryService.GetGalleriesForIndex();
+
+            return View("SiteMenoViewComponent", gallery);
+        }
+
+        #endregion
+    }
+}
