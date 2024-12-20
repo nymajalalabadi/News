687e569943d7c779a5e4cb0f129b2d5a3337f471
nym jalalabadi <nima.jalalabadii@gmail.com>
2024-12-20 15:02:16 -0600

    Added Link Model

---
+++ News.DataLayer/Migrations/20241220210156_AddedLinkTbl.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace News.DataLayer.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddedLinkTbl : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
---
+++ News.Domain/Entities/Account/Links.cs
@@ -0,0 +1,22 @@
+﻿using News.Domain.Entities.Common;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace News.Domain.Entities.Account
+{
+    public class Links : BaseEntity
+    {
+        [Display(Name = "نام لینک")]
+        public string? LinkName { get; set; } = default!;
+        [Display(Name = "آدرس لینک")]
+        public string? LinkAddress { get; set; } = default!;
+
+        [Display(Name = "وضعیت")]
+        public bool IsSuccess { get; set; } = false;
+    }
+}
